82d22d9a60ff409f24062de4752385d63099639b
eman khalaf <eman.mufeed.khak@ninjavan.co>
2022-12-07 23:43:27 +0200

    Welcome Page

---
+++ app/src/main/java/com/example/androidstudioproject/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.androidstudioproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import java.util.Objects;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private Animation flying;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        setContentView(R.layout.activity_main);
+        img = findViewById(R.id.img);
+        flying = AnimationUtils.loadAnimation(this, R.anim.flying_animation);
+        img.setAnimation(flying);
+
+    }
+}
