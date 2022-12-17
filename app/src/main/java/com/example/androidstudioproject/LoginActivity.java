744b939ae65ac9426b10987c2dbf892ae5bfc6fa
eman khalaf <eman.mufeed.khak@ninjavan.co>
2022-12-17 16:32:34 +0200

    show message error

--- app/src/main/java/com/example/androidstudioproject/LoginActivity.java
+++ app/src/main/java/com/example/androidstudioproject/LoginActivity.java
@@ -7,24 +7,60 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
     private TextView textViewSignup ;
+    private EditText inputEmail,inputPassword;
+    private Button loginBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide(); //<< this
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-        textViewSignup = findViewById(R.id.textViewSignUp);
+        setupViews();
         textViewSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
             }
         });
+    }
+    public void checkCredentials(){
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+
+        if(email.isEmpty() || !email.contains("@")){
+            showError(inputEmail,"Email is not valid");
+        }
+        else if(password.isEmpty() || password.length()<7){
+            showError(inputPassword,"Password must be 7 character");
+        }else {
+            startActivity(new Intent(LoginActivity.this,HomePageActivity.class));
+        }
+
+    }
+
+    private void showError(EditText input, String error) {
+        input.setError(error);
+        input.requestFocus();
+    }
+
+
+
+    public void setupViews(){
+        textViewSignup = findViewById(R.id.textViewSignUp);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputEmail = findViewById(R.id.inputEmail);
+        loginBtn = findViewById(R.id.btnLogin);
+    }
+
+    public void btnLogin(View view) {
+        checkCredentials();
     }
 }
--- app/src/main/java/com/example/androidstudioproject/RegisterActivity.java
+++ app/src/main/java/com/example/androidstudioproject/RegisterActivity.java
@@ -6,7 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -14,15 +17,22 @@ import java.util.Objects;
 
 public class RegisterActivity extends AppCompatActivity {
     private TextView textViewLogin ;
-
+    private EditText inputUserName,inputPassword,inputEmail,inputConfirmPassword;
+    private Button btnRegister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(getSupportActionBar()).hide(); //<< this
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_register);
+        setupViews();
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCredentials();
+            }
+        });
 
-        textViewLogin = findViewById(R.id.alreadyHaveAccount);
         textViewLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,4 +40,45 @@ public class RegisterActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void checkCredentials() {
+        String userName = inputUserName.getText().toString();
+        String email = inputEmail.getText().toString();
+        String password = inputPassword.getText().toString();
+        String confirmPassword = inputConfirmPassword.getText().toString();
+
+        if(userName.isEmpty() || userName.length()< 7){
+            showError(inputUserName,"Your username is not valid");
+        }
+        else if(email.isEmpty() || !email.contains("@")){
+            showError(inputEmail,"Email is not valid");
+        }
+        else if(password.isEmpty() || password.length()<7){
+            showError(inputPassword,"Password must be 7 character");
+        }
+        else if (confirmPassword.isEmpty() || !confirmPassword.equals(password)){
+            showError(inputConfirmPassword,"Password not match");
+        }else {
+            startActivity(new Intent(RegisterActivity.this,HomePageActivity.class));
+        }
+
+
+    }
+
+    private void showError(EditText input, String error) {
+        input.setError(error);
+        //it will help us to show the error
+        input.requestFocus();
+
+    }
+
+    public void setupViews(){
+        textViewLogin = findViewById(R.id.alreadyHaveAccount);
+        inputUserName = findViewById(R.id.inputUserName);
+        inputPassword = findViewById(R.id.inputPassword);
+        inputConfirmPassword = findViewById(R.id.inputConfirmPassword);
+        inputEmail = findViewById(R.id.inputEmail);
+        btnRegister = findViewById(R.id.btnRegister);
+    }
+
 }
